bbb6a1fc4c1245ae2aac933967a9fab3cf217f76
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-06-15 22:56:49 -0400

    I have committed quadruple homicide

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,39 +1,66 @@
 import os, ctypes, random
+from playsound import playsound
 
 if os.name =='nt':
 	os.system('cls')
 	ctypes.windll.kernel32.SetConsoleTitleW(f"Rock! Paper! Scissors!")
 
-choices = ["Rock", "Paper", "Scissors"]
-result = ["Tied!", "Lost!", "Won!"]
-botChoice = random.choice(choices)
-userChoice = (input("Choose one! Rock, Paper or Scissors\n"))
+userScore, botScore = 0, 0
 
-if userChoice not in choices:
-    print("That is not a valid option!")
-    exit()
+while True:
+	choices = ["Rock", "Paper", "Scissors"]
+	result = ["Tied!", "Lost!", "Won!"]
+	invalidSongs = ["android", "omg"]
+	idiot = random.choice(invalidSongs)
+	botChoice = random.choice(choices)
+	os.system('cls')
+	userChoice = (input("Choose one! Rock, Paper or Scissors\n"))
 
-elif userChoice == botChoice:
-    result = result[0]
+	if userChoice not in choices:
+		playsound(f'sounds/{idiot}.wav')
+		print("That is not a valid option!")
+		exit()
 
-elif userChoice == choices[0] and botChoice == choices[1]:
-    result = result[1]
+	elif userChoice == botChoice:
+		result = result[0]
+		playsound('sounds/bruh.wav')
 
-elif userChoice == choices[0] and botChoice == choices[2]:
-    result = result[2]
+	elif userChoice == choices[0] and botChoice == choices[1]:
+		result = result[1]
+		botScore += 1
+		playsound('sounds/fart.wav')
 
-elif userChoice == choices[1] and botChoice == choices[2]:
-    result = result[1]
+	elif userChoice == choices[0] and botChoice == choices[2]:
+		result = result[2]
+		userScore += 1
+		playsound('sounds/lessgo.wav')
 
-elif userChoice == choices[1] and botChoice == choices[0]:
-    result = result[2]
+	elif userChoice == choices[1] and botChoice == choices[2]:
+		result = result[1]
+		botScore += 1
+		playsound('sounds/fart.wav')
 
-elif userChoice == choices[2] and botChoice == choices[0]:
-    result = result[1]
+	elif userChoice == choices[1] and botChoice == choices[0]:
+		result = result[2]
+		userScore += 1
+		playsound('sounds/lessgo.wav')
 
-elif userChoice == choices[2] and botChoice == choices[1]:
-    result = result[2]
+	elif userChoice == choices[2] and botChoice == choices[0]:
+		result = result[1]
+		botScore += 1
+		playsound('sounds/fart.wav')
 
-os.system('cls')
-print(f"Your Choice: {userChoice}\nBot Choice: {botChoice}\nYou {result}")
-exit()
+	elif userChoice == choices[2] and botChoice == choices[1]:
+		result = result[2]
+		userScore += 1
+		playsound('sounds/lessgo.wav')
+
+	os.system('cls')
+	print(f"Your Choice: {userChoice} | Bot Choice: {botChoice}\nYou {result}\n\nYour Score: {userScore} | Bot Score: {botScore}")
+
+	retry = input("Would you like to play again? (Y/N): ")
+	if retry not in ["y", "Y", "Yes"]:
+		print("See you next time!")
+		break
+	else:
+		playsound("sounds/yeahyeah.wav")
